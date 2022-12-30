46e7c5ada1333dae3e99404623344377249568dd
Juratbek0128 <114687223+Juratbek0128@users.noreply.github.com>
2022-12-30 22:38:51 +0500

    if.py

---
+++ if.py
@@ -0,0 +1,211 @@
+
+'''juratbekotajonoff'''
+
+# ###if1
+# a=int(input())
+# if a>0:
+#     print(a+1)
+# else:
+#     print(a)
+# ###if2
+# a=int(input())
+# if a>0:
+#     print(a+1)
+# else:
+#     print(a-2)
+# ###if3
+# a=int(input())
+# if a>0:
+#     print(a+1)
+# elif a<0:
+#     print(a-2)
+# elif a==0:
+#     print(10)
+# else:
+#     print(a)
+# ###if4
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# list1 = [a,b,c]
+# pos_count = 0
+# for num in list1:
+#     if num >= 0:
+#         pos_count += 1
+#     else:      
+#         break
+# print("musbat: ", pos_count)
+# ###if5
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# list1 = [a,b,c]
+# pos_count, neg_count = 0, 0
+# for num in list1:
+#     if num >= 0:
+#         pos_count += 1
+#     else:
+#         neg_count += 1
+# print("musbat ", pos_count)
+# print("manfiy ", neg_count)
+# ###if6
+# a=int(input())
+# b=int(input())
+# c=[a,b]
+# d=max(c)
+# print(f"2 ta sonning katta si shu son {d}")
+# ###if7
+# a=int(input())
+# b=int(input())
+# c=[a,b]
+# d=min(c)
+# print(d,0)
+# ###if8
+# a=int(input())
+# b=int(input())
+# c=[a,b]
+# d=max(c)
+# f=min(c)
+# print(d,f)
+# ###if9
+# a=float(input())
+# b=float(input())
+# if a<b:
+#     print(a)
+# else:
+#     print(b)
+# ###if10
+# a=int(input())
+# b=int(input())
+# if a!=b:
+#     print(a+b)
+# else:
+#     print(0)
+# print(a,b)
+# ###if11
+# a=int(input())
+# b=int(input())
+# c=[a,b]
+# d=max(c)
+# e=min(c)
+# if a!=b: 
+#     print(d)
+# else:
+#     print(0)
+# print(a,b)
+# ###if12
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=[a,b,c]
+# print(min(d))
+# ###if13
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=(a+b+c)/3
+# print(d)
+# ###if14
+# a=int(input())
+# b=int(input())
+# c=[a,b]
+# d=max(c)
+# e=min(c)
+# if a!=b: 
+#     print(d)
+# else:
+#     print(0)
+# print(a,b)
+# ###if15
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=[a,b,c]
+# e=max(d)
+# f=(a+b+c)/3
+# print(e+f," \n ", e,f )
+# ###if16
+# import numpy as np
+# a=float(input())
+# b=float(input())
+# c=float(input())
+# d=[a,b,c]
+# if sorted(d):
+#     print(a+b+c/2)
+# else:
+#     print(np.sign(d))
+# ###if17
+# a=float(input())
+# b=float(input())
+# c=float(input())
+# d=[a,b,c]
+# if sorted(d):
+#     print(a+b+c/2)
+# else:
+#     print(a,b,c)
+# ###if18
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=[a,b,c]
+# e=d.index(c)
+# if a==b:
+#     print(e)
+# else:
+#     print('2 tasi teng emas')
+# ###if19
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# m=int(input())
+# d=[a,b,c,m]
+# e=d.index(m)
+# if a==b==c:
+#     print(e)
+# else:
+#     print('2 tasi teng emas')
+# ###if20
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if c>b:
+#     print(a-c)
+# ###if21
+# a=int(input())
+# if a==0:
+#     print(0)
+# elif 0<a<0:
+#     print(1,2)
+# else:
+#     print(3)
+# ###if22
+# x = int(input())
+# y = int(input())
+# print("(",x,",",y,")",end="")
+# if(x<0 and y<0):
+#     print("3 chorak")
+# elif(x<0 and y>0):
+#     print("1 chorak")
+# elif(x>0 and y>0):
+#     print("2 chorak")
+# else:
+#     print("4 chorak")
+# ....
+# ###if29
+# a=int(input())
+# if a%2!=0:
+#     print( "toq son" )
+# elif a%2==0:
+#     print('juft son')
+# elif a==0:
+#     print('nolga teng')
+# ###if30
+# for i in range(1,999):
+#     print(i)
+#     if i %2==0:
+#         print("juft",(len(str(i)), "xonali son"))
+#     else:
+#         print('toq',(len(str(i)), "xonali son"))
+
+
+
